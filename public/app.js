44d5d9a6e976c1574df9594ec7af02243a3d5d47
slowlife25 <slowlife25@gmail.com>
2018-08-28 02:15:17 +0900

    test
    ;
    q
    ;

---
+++ public/app.js
@@ -0,0 +1,17 @@
+'use strict';
+var learnjs = {};
+
+learnjs.problemView = function() {
+  return $('<div class="problem-view">').text('coming soon');
+}
+
+learnjs.showView = function(hash) {
+  var routes = {
+    '#problem':learnjs.problemView
+  };
+  var hashParts = hash.split('-');
+  var viewFn = routes[hashParts[0]];
+  if (viewFn) {
+    $('.view-container').empty().append(viewFn(hashParts[1]));
+  }
+}
